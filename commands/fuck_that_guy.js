46eec7d752efc3a9bd24927fef879a93533430fc
Audrey <Audrey.JACD@gmail.com>
2020-04-12 20:25:03 -0600

    Added fuck_that_guy command.

---
+++ commands/fuck_that_guy.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'fuck',
+	description: 'Just fuck \'em',
+	execute(message, args) {
+		if (!args[0]) return;
+
+		const member = message.mentions.members.first();
+		if (!member) return;
+
+		return message.channel.send(`Yeah! Fuck ${member.nick}`);
+	},
+};

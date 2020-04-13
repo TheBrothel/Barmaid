e99218bbad83b906d5154eb6b542cc5a273d0402
Audrey <Audrey.JACD@gmail.com>
2020-04-12 19:48:59 -0600

    Testing dynamic commands.

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const config = require('./config.json');
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Load all command files.
+const commandFiles = fs.readdirSync('./commands')
+    .filter(file => file.endsWith('.js'));
+
+// Add the commands to our client.
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // Set a new item in the Collection with the 
+    // key as the command name and the value as the exported module.
+    client.commands.set(command.name, command);
+}
+
 
 client.once('ready', () => {
+    // Let us know you're alive in the console.
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	console.log(message.content);
+    // Only process the message if it starts with our prefix
+    // or it's from a bot.
+    if (!message.content.startsWith(prefix) || message.author.bot) 
+        return;
+
+    // Grab the argument list.
+    const args = message.content.slice(prefix.length).split(/ +/);
+    
+    // Grab the normalized command name.
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
-client.login(process.env.BOT_TOKEN || config.token);
+
+
+client.login(process.env.BOT_TOKEN || token);

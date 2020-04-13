1e8862ac1b280d993d80de979024db9b719707f8
Audrey <Audrey.JACD@gmail.com>
2020-04-13 17:05:21 -0600

    booru class refactoring and expansion.

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { parse } = require('node-html-parser');
 const Discord = require('discord.js');
 
 module.exports = {
@@ -23,7 +24,24 @@ module.exports = {
 	},
 };
 
+// Base class for all image boards.
 class ImageBoard {
+	displayName;
+	message;
+	baseUrl;
+	searchUrl;
+	postUrl;
+	forcedTags;
+	maxTags;
+	post = {
+		id: null,
+		image_url: null,
+		score: null,
+		artist: null,
+		copyright: null,
+		character: null,
+	};
+
 	constructor(
 		message, 
 		displayName,
@@ -42,11 +60,14 @@ class ImageBoard {
 		this.maxTags = maxTags;
 	}
 
+	// Checks if the requested and enforced tags are under the allowed limit.
 	canSearch(tags) {
-		return this.maxTags <= 0 
-			|| (this.forcedTags.length + tags.length) <= this.maxTags;
+		const allTagsCount = this.forcedTags.length + tags.length;
+
+		return this.maxTags <= 0 || allTagsCount<= this.maxTags;
 	}
 
+	// Performs the search for the given tags.
 	search(tags) {
 		const fullTags = [...this.forcedTags, ...tags];
 
@@ -56,23 +77,26 @@ class ImageBoard {
 			return;
 		}
 
-		axios.get(this.searchUrl + fullTags.join(' '))
-			.then(response => {
-				const post = response.data.random();
-
-				if(!post){
-					this.message.channel.send(`No results found on ${this.DisplayName} :frowning:`);
-
-					return;
-				}
-
-				this.embedPost(post);
-			})
-			.catch(error => {
-				console.log(error);
-			});
+		this.doSearch(fullTags);
 	}
 
+	// Site-specific logic to build the search URL with tags.
+	//
+	// Overridable.
+	buildSearchURL(tags) {
+		return this.searchUrl + tags.join(' ');
+	}
+
+	// Site-specific logic to issue the network requests 
+	// and parse the responses.
+	//
+	// Overridable.
+	doSearch(tags) {
+		return null;
+	}
+
+	// Builds and sends the Discord Embed object 
+	// based on the search results.
 	embedPost(post) {
 		const postEmbed = new Discord.MessageEmbed()
 			.setTitle(this.displayName)
@@ -85,12 +109,72 @@ class ImageBoard {
 		this.message.channel.send(postEmbed);
 	}
 
-	buildFooter(post){
-		return null;
+	// Builds the site-specific field strings.
+	//
+	// Overridable.
+	buildFields(post, embed) {
+		return embed;
+	}
+
+	// Builds the site-specific post URL.
+	//
+	// Overridable.
+	buildPostURL(post) {
+		return this.postUrl;
+	}
+
+	sendNoResultsError(tags) {
+		return this.message.channel.send(`No results found on ${this.DisplayName} for \`${tags}\` :frowning:`);
 	}
 };
 
-class Gelbooru extends ImageBoard  {
+// Image boards that allow a JSON response.
+class JSONImageBoard extends ImageBoard {
+	doSearch(tags) {
+		axios.get(this.buildSearchURL(tags))
+		.then(response => {
+			this.post = this.parseSearchResponse(response);
+
+			this.embedPost(this.post);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	}
+
+	parseSearchResponse(response) {
+		return this.post;
+	}
+
+	embedPost(post) {
+		let postEmbed = new Discord.MessageEmbed()
+			.setTitle(this.displayName)
+			.setURL(this.buildPostURL(post))
+			.setImage(post.image_url);
+
+		postEmbed = this.buildFields(post, postEmbed);
+
+		this.message.channel.send(postEmbed);
+	}
+
+	buildFields(post, embed) {
+		if(post.copyright)
+			embed.addField('Copyright', post.copyright, true);
+			
+		if(post.character)
+			embed.addField('Character', post.character, true);
+			
+		if(post.artist)
+			embed.addField('Artist', post.artist, true);
+
+		if(post.score)
+			embed.addField('Score', post.score, true);
+
+		return embed;
+	}
+}
+
+class Gelbooru extends JSONImageBoard  {
 	constructor(message) {
 		const baseUrl = 'https://gelbooru.com/index.php?page=';
 		const searchUrl = baseUrl + 'dapi&s=post&q=index&json=1&tags=';
@@ -117,12 +201,27 @@ class Gelbooru extends ImageBoard  {
 		);
 	}
 
-	buildFooter(post) {
-		return `Score: ${post.score}`;
+	parseSearchResponse(response) {
+		const firstResponse = response.data.random();
+
+		if(!firstResponse) return this.sendNoResultsError(tags);
+
+		return {
+			id: firstResponse.id,
+			image_url: firstResponse.file_url,
+			score: firstResponse.score,
+			artist: null,
+			copyright: null,
+			character: null,
+		};
+	}
+
+	buildPostURL(post) {
+		return this.postUrl + post.id;
 	}
 }
 
-class Danbooru extends ImageBoard  {
+class Danbooru extends JSONImageBoard  {
 	constructor(message) {
 		const baseUrl = 'https://danbooru.donmai.us/';
 		const searchUrl = baseUrl + 'posts.json?random=1&tags=';
@@ -143,9 +242,22 @@ class Danbooru extends ImageBoard  {
 		);
 	}
 
-	buildFooter(post) {
-		return `Score: ${post.up_score - post.down_score}`
-			+ ` | Copyright: ${post.tag_string_copyright}`
-			+ ` | Artist: ${post.tag_string_artist}`;
+	parseSearchResponse(response) {
+		const firstResponse = response.data.random();
+
+		if(!firstResponse) return this.sendNoResultsError(tags);
+
+		return {
+			id: firstResponse.id,
+			image_url: firstResponse.large_file_url || firstResponse.file_url,
+			score: firstResponse.up_score - firstResponse.down_score,
+			artist: firstResponse.tag_string_artist,
+			copyright: firstResponse.tag_string_copyright,
+			character: firstResponse.tag_string_character,
+		};
+	}
+
+	buildPostURL(post) {
+		return this.postUrl + post.id;
 	}
 }

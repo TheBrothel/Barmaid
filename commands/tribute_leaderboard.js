f6197e64ed4d221930d56364a543698ee93b84d5
Noah Martino <personal.boredhero@gmail.com>
2020-04-15 19:10:15 -0400

    Did some work on the tribute_leaderboard command

--- commands/tribute_leaderboard.js
+++ commands/tribute_leaderboard.js
@@ -6,6 +6,24 @@ module.exports = {
     description: 'Whos the sluttiest of them all?',
     execute(message, args){
       //TODO: Add the fucking command lmao
+      let topFiveUsers = [];
+      let cumCounts = [];
+
+      DB.query(`SELECT target_id, SUM(times) FROM cum_tribute_data GROUP BY target_id ORDER BY SUM(times)`,
+        (rows, fields) => {
+          for(let i = 0; i < rows.length; i++){
+            console.log(rows[i]);
+            topFiveUsers.push(rows[i]['target_id']);
+            cumCounts.push(rows[i]['SUM(times)']);
+          }
+        }
+      );
+
+      for(let i = 0; i < topFiveUsers.length; i++){
+        console.log('I ran.');
+         console.log(`${topFiveUsers[i]} : ${cumCounts[i]}`);
+      }
+
       return message.channel.send('This is a stub for the tribute_leaderboard command.')  
     },
 };

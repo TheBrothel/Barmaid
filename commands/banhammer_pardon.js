efb043ef5c73025034d120faa5d2ddf031514e48
Noah Martino <personal.boredhero@gmail.com>
2020-04-16 00:30:21 -0400

    Started work on banhammer commands

---
+++ commands/banhammer_pardon.js
@@ -0,0 +1,18 @@
+const { parseMentions } = require('../message-utils.js');
+const { DB } = require('../db.js');
+
+module.exports = {
+    name: 'banhammer_pardon',
+    description: 'One slut to rule them all.'
+    this.execute(message, args){
+        //Get stuff we need to work with
+        const author = message.author;
+        const mentions = parseMentions(message);
+        const firstMention = mentions[0];
+        //Check to make sure we are dealing with Audrey
+        if(author.id != '400464877925171201'){
+            return message.channel.send(`Only the supreme cumdump may utilize this sacred weapon. You are not worthy.`);
+        }
+        //TOOD: Talk to the database
+    },
+};

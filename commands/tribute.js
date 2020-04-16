d07147a8e3ddb98ad42c65a6c9fb66462e35f849
Audrey <Audrey.JACD@gmail.com>
2020-04-15 21:37:59 -0600

    Fix tribute message formatting.

--- commands/tribute.js
+++ commands/tribute.js
@@ -43,16 +43,16 @@ module.exports = {
             'face': {
                 'tags': ['cum_on_face', '-vaginal', '-anal'],
                 'messages': [
-                    `${author.username} grabs ${firstMention.username}'s hair and pulls their head back to watch \
-                    as ${author.username} uses their other hand to stroke their rigid cock right over \
-                    ${firstMention.username}'s face. \n\n \
-                    Before long, ${author.username}'s cock leaks precum onto ${firstMention.username}'s face \
-                    as ${author.username} tenses. They pull back on ${firstMention.username}'s hair more and \
-                    unloads thick ropes of cum across their face with a satisfied groan.`,
+                    `${author.username} grabs ${firstMention.username}'s hair and pulls their head back to watch`
+                    + ` as ${author.username} uses their other hand to stroke their rigid cock right over`
+                    + ` ${firstMention.username}'s face. \n\n`
+                    + ` Before long, ${author.username}'s cock leaks precum onto ${firstMention.username}'s face`
+                    + ` as ${author.username} tenses. They pull back on ${firstMention.username}'s hair more and`
+                    + ` unloads thick ropes of cum across their face with a satisfied groan.`,
 
-                    `With a relieved groan, ${author.username} gives their needy cock a few more furious pumps \
-                    before firing a sticky shot of cum across the ${firstMention.username}'s face from hair to chin, \
-                    letting the next few pumps fly carelessly into ${firstMention.username}'s mouth and cheeks.`,
+                    `With a relieved groan, ${author.username} gives their needy cock a few more furious pumps`
+                    + ` before firing a sticky shot of cum across the ${firstMention.username}'s face from hair to chin,`
+                    + ` letting the next few pumps fly carelessly into ${firstMention.username}'s mouth and cheeks.`,
                 ],
             },
             'eyes': { 'redirect': 'face' },
@@ -64,10 +64,10 @@ module.exports = {
             'mouth': {
                 'tags': ['cum_in_mouth', '-vaginal', '-anal'],
                 'messages': [
-                    `${author.username} drags ${firstMention.username}'s lips over the head of their \
-                    cock by ${firstMention.username}'s hair, jerking off eagerly with their other hand. \
-                    Soon enough, ${author.username}'s balls tighten and they empty their balls straight into \
-                    ${firstMention.username}'s sucking mouth, filling the warm hole with delicious cum.`,
+                    `${author.username} drags ${firstMention.username}'s lips over the head of their`
+                    + ` cock by ${firstMention.username}'s hair, jerking off eagerly with their other hand.`
+                    + ` Soon enough, ${author.username}'s balls tighten and they empty their balls straight into`
+                    + ` ${firstMention.username}'s sucking mouth, filling the warm hole with delicious cum.`,
                 ],
             },
             'tongue': { 'redirect': 'mouth' },
@@ -77,10 +77,10 @@ module.exports = {
             'breasts': {
                 'tags': ['cum_on_breasts', '-vaginal', '-anal'],
                 'messages': [
-                    `${author.username} aims their cock at ${firstMention.username}'s exposed tits, jerking \
-                    the rigid shaft eagerly as their climax builds. A thick spray of pent-up cum glazes \
-                    ${firstMention.username}'s chest as ${author.username} keeps pumping, a large load \
-                    soon dripping down ${firstMention.username}'s body.`,
+                    `${author.username} aims their cock at ${firstMention.username}'s exposed tits, jerking`
+                    + ` the rigid shaft eagerly as their climax builds. A thick spray of pent-up cum glazes`
+                    + ` ${firstMention.username}'s chest as ${author.username} keeps pumping, a large load`
+                    + ` soon dripping down ${firstMention.username}'s body.`,
                 ],
             },
             'tits': { 'redirect': 'breasts' },
@@ -90,18 +90,18 @@ module.exports = {
             'pussy': {
                 'tags': ['cum_in_pussy', '-anal'],
                 'messages': [
-                    `${author.username} holds ${firstMention.username} by the hips tightly as they \
-                    thrust with desperate need. Within moments, ${author.username} bottoms out in \
-                    ${firstMention.username}'s pussy roughly before arching their back and emptying \
-                    hot ropes of cum deep inside her, excess semen quickly leaking \
-                    out around where the two are connected.`,
+                    `${author.username} holds ${firstMention.username} by the hips tightly as they`
+                    + ` thrust with desperate need. Within moments, ${author.username} bottoms out in`
+                    + ` ${firstMention.username}'s pussy roughly before arching their back and emptying`
+                    + ` hot ropes of cum deep inside her, excess semen quickly leaking`
+                    + ` out around where the two are connected.`,
 
-                    `${author.username} bends ${firstMention.username} over against the wall, kicking her feet \
-                    further apart and teasing the head of their rigid cock up and down her pussy lips. With \
-                    an unceremonious thrust forward, ${author.username} stretches ${firstMention.username} over \
-                    themself like a sleeve and starts using her cunt to milk their cock. Before long, ${author.username} \
-                    drives into her hard enough to stumble her forward as they relieve their heavy balls, splashing \
-                    thick cum deep inside her.`,
+                    `${author.username} bends ${firstMention.username} over against the wall, kicking her feet`
+                    + ` further apart and teasing the head of their rigid cock up and down her pussy lips. With`
+                    + ` an unceremonious thrust forward, ${author.username} stretches ${firstMention.username} over`
+                    + ` themself like a sleeve and starts using her cunt to milk their cock. Before long, ${author.username}`
+                    + ` drives into her hard enough to stumble her forward as they relieve their heavy balls, splashing`
+                    + ` thick cum deep inside her.`,
                 ],
             },
             'cunt': { 'redirect': 'pussy' },
@@ -116,16 +116,16 @@ module.exports = {
             'ass': {
                 'tags': ['cum_in_ass', '-vaginal'],
                 'messages': [
-                    `${author.username} pulls back on ${firstMention.username}'s hips roughly as they fuck, \
-                    ${firstMention.username}'s ass stretched pleasurable over ${author.username}'s needy cock. \
-                    ${author.username} lets out a deep groan as they start pouring their hot load into the tight \
-                    hole, not stopping their hips until ${firstMention.username}'s ass is leaking fresh cum down \
-                    their thighs.`,
+                    `${author.username} pulls back on ${firstMention.username}'s hips roughly as they fuck,`
+                    + ` ${firstMention.username}'s ass stretched pleasurable over ${author.username}'s needy cock.`
+                    + ` ${author.username} lets out a deep groan as they start pouring their hot load into the tight`
+                    + ` hole, not stopping their hips until ${firstMention.username}'s ass is leaking fresh cum down`
+                    + ` their thighs.`,
 
-                    `As ${author.username} pounds away at ${firstMention.username}'s ass with loud smacks of their skin, \
-                    ${author.username} suddenly tenses up and drives their cock down into the abused fuckhole with their \
-                    weight. Thick sprays of cum paint the inside of ${firstMention.username}'s body as ${author.username}'s \
-                    cock pulses with relief.`,
+                    `As ${author.username} pounds away at ${firstMention.username}'s ass with loud smacks of their skin,`
+                    + ` ${author.username} suddenly tenses up and drives their cock down into the abused fuckhole with their`
+                    + ` weight. Thick sprays of cum paint the inside of ${firstMention.username}'s body as ${author.username}'s`
+                    + ` cock pulses with relief.`,
                 ],
             },
             'asshole': { 'redirect': 'ass' },
@@ -133,10 +133,10 @@ module.exports = {
             'ear': {
                 'tags': ['ear_insertion', '-tentacles', '-slugs'],
                 'messages': [
-                    `${author.username} grabs ${firstMention.username}'s head in both hands, covering ${firstMention.username}'s \
-                    face with their palm as they roughly rub their hard cock against ${firstMention.username}'s defenseless ear. \
-                    Eventually, ${author.username} pulls their hips back enough to press the head of their leaking cock into the
-                    exposed ear and unloads waves of hot, sticky cum, trying to soak ${firstMention.username}'s brain in it directly.`,
+                    `${author.username} grabs ${firstMention.username}'s head in both hands, covering ${firstMention.username}'s`
+                    + ` face with their palm as they roughly rub their hard cock against ${firstMention.username}'s defenseless ear.`
+                    + ` Eventually, ${author.username} pulls their hips back enough to press the head of their leaking cock into the`
+                    + ` exposed ear and unloads waves of hot, sticky cum, trying to soak ${firstMention.username}'s brain in it directly.`,
                 ],
             },
         };

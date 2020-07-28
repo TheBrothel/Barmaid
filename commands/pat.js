db886eb1189b4331e3825f35ec0e16b61044d4d4
Noah Martino <personal.boredhero@gmail.com>
2020-07-27 20:54:42 -0400

    pat adjustments

--- commands/pat.js
+++ commands/pat.js
@@ -9,29 +9,32 @@ module.exports = {
     description: 'Imoutos are for protecting and headpatting.',
     async execute(message, args){
         
+        //Get stuff to work with
+        //const author = message.author;
+        //const authorName = author.displayName;
+        //const authorName = messsage.member.displayName;
+        //const mentions = parseMentions(message);
+        // const firstMention = mentions[0];
+        //const mentionName = firstMention.displayName;
+
         const author = message.author;
         const firstMention = await [...message.mentions.members][0][1].fetch();
         const authorName = message.member.displayName;
         const mentionName = firstMention.displayName;
 
-
         const site = new Gelbooru(message);
-        const siteSearch = site.search('head_pat');
+        const post = await site.search(['head_pat', '-loli']);
+
         
         //Check to make sure this is a valid command
         if(args.length < 1 || args.length > 1){
             console.log('The correct syntax of this command is "$pat @user", aliases $headpat and $pet also work.');
         }
         else{
-            siteSearch.then((post) => {
-                let embed = new Discord.MessageEmbed()
-                    .setDescription(`${authorName} gingerly pats ${mentionName} on the head.`)
-                    .setImage(post.image_url)
-                    .setFooter(site.buildPostURL(post));
-
-
+            let embed = new Discord.MessageEmbed()
+                .setDescription(`${authorName} gingerly pats ${mentionName} on the head.`)
+                .setImage(post.image_url);
             message.channel.send(embed);
-            })
         }
     }
 }
